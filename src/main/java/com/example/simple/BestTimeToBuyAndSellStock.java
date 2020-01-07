337ad66f847520409c6faaab453b723858e4c69b
1213 <allen@123.com>
2020-01-07 19:12:32 +0800

    [add]simple 121

--- src/main/java/com/example/simple/BestTimeToBuyAndSellStock.java
+++ src/main/java/com/example/simple/BestTimeToBuyAndSellStock.java
@@ -6,12 +6,29 @@ package com.example.simple;
 public class BestTimeToBuyAndSellStock {
 
     public static int maxProfit(int[] prices) {
-        return 0;
+
+        if (prices.length <= 1) {
+            return 0;
+        }
+
+        int min = prices[0];
+        int max = 0;
+        for (int i = 1; i < prices.length; i++) {
+
+            if (prices[i] < min) {
+                min = prices[i];
+            } else if (prices[i] - min > max) {
+                max = prices[i] - min;
+            }
+
+        }
+
+        return max;
     }
 
     public static void main(String[] args) {
         int[] prices = {7,1,5,3,6,4};
-        maxProfit(prices);
+        System.out.println(maxProfit(prices));
     }
 }
 
